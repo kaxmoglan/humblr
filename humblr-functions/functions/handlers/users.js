36ee020635907cf765be3ecc209377af57c20487
Maximilian Coghlan <max.coghlan@icloud.com>
2020-10-07 15:56:44 +0100

    image upload functionality

--- humblr-functions/functions/handlers/users.js
+++ humblr-functions/functions/handlers/users.js
@@ -1,4 +1,4 @@
-const { db } = require("../util/admin");
+const { admin, db } = require("../util/admin");
 const config = require("../util/config");
 const { validateSignUp, validateLogin } = require("../util/validators");
 const firebase = require("firebase");
@@ -17,6 +17,8 @@ exports.signup = (req, res) => {
   if (!valid) return res.status(400).json({ errors });
 
   // Validation passed
+
+  const noImg = "default_profile.png";
   let token, userId;
 
   db.doc(`/users/${newUser.username}`)
@@ -45,6 +47,7 @@ exports.signup = (req, res) => {
         username: newUser.username,
         email: newUser.email,
         createdAt: new Date().toISOString(),
+        imageUrl: `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${noImg}?alt=media`,
         userId,
       };
       return db.doc(`/users/${newUser.username}`).set(userCredentials);
@@ -86,4 +89,55 @@ exports.login = (req, res) => {
         return res.status(500).json({ error: err.code });
       }
     });
+};
+
+exports.uploadImage = (req, res) => {
+  const BusBoy = require("busboy");
+  const path = require("path");
+  const os = require("os");
+  const fs = require("fs");
+
+  const busboy = new BusBoy({ headers: req.headers });
+
+  let imageFileName;
+  let imageToBeUploaded = {};
+
+  busboy.on("file", (fieldName, file, filename, encoding, mimetype) => {
+    if (mimetype !== "image/jpeg" && mimetype !== "image/png") {
+      return res.status(400).json({ error: "Wrong file type submitted" });
+    }
+    const imageExtension = filename.split(".")[filename.split(".").length - 1];
+    imageFileName = `${Math.round(
+      Math.random() * 1000000000
+    )}.${imageExtension}`;
+    const filePath = path.join(os.tmpdir(), imageFileName);
+    imageToBeUploaded = { filePath, mimetype };
+    file.pipe(fs.createWriteStream(filePath));
+  });
+  busboy.on("finish", () => {
+    admin
+      .storage()
+      .bucket()
+      .upload(imageToBeUploaded.filePath, {
+        resumable: false,
+        metadata: {
+          metadata: {
+            contentType: imageToBeUploaded.mimetype,
+          },
+        },
+      })
+      .then(() => {
+        const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${imageFileName}?alt=media`;
+        return db.doc(`/users/${req.user.username}`).update({ imageUrl });
+      })
+      .then(() => {
+        return res.json({ message: "Image uploaded successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+      });
+  });
+
+  busboy.end(req.rawBody);
 };
--- humblr-functions/functions/index.js
+++ humblr-functions/functions/index.js
@@ -3,16 +3,15 @@ const app = require("express")();
 
 const FBAuth = require("./util/fbAuth");
 const { getAllMurmurs, postMurmur } = require("./handlers/murmurs");
-const { signup, login } = require("./handlers/users");
+const { signup, login, uploadImage } = require("./handlers/users");
 
 // MURMUR ROUTES
 app.get("/murmurs", getAllMurmurs);
 app.post("/murmur", FBAuth, postMurmur);
 
-// SIGNUP ROUTE
+// USERS ROUTES
 app.post("/signup", signup);
-
-// LOGIN ROUTE
 app.post("/login", login);
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.region("europe-west1").https.onRequest(app);
