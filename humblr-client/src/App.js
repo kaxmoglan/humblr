ed6de22900c1a59be7df1ba3f1119dfb7854ab32
Maximilian Coghlan <max.coghlan@icloud.com>
2020-10-13 15:26:56 +0100

    redirect from login/signup to home if logged in

--- humblr-client/src/App.js
+++ humblr-client/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import jwtDecode from "jwt-decode";
+
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import ThemeFile from "./util/Theme";
+import AuthRoute from "./util/AuthRoute";
 
 // COMPONENTS
 import NavBar from "./components/NavBar";
@@ -15,6 +19,19 @@ import Signup from "./pages/Signup";
 // CUSTOM MATERIAL THEME
 const theme = createMuiTheme(ThemeFile);
 
+const token = localStorage.FBIdToken;
+let authenticated;
+if (token) {
+  const decodedToken = jwtDecode(token);
+
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
+
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
@@ -23,9 +40,19 @@ function App() {
           <NavBar />
           <div className="container">
             <Switch>
-              <Route exact path="/" component={Home}></Route>
-              <Route exact path="/login" component={Login}></Route>
-              <Route exact path="/signup" component={Signup}></Route>
+              <Route exact path="/" component={Home} />
+              <AuthRoute
+                exact
+                path="/login"
+                component={Login}
+                authenticated={authenticated}
+              />
+              <AuthRoute
+                exact
+                path="/signup"
+                component={Signup}
+                authenticated={authenticated}
+              />
             </Switch>
           </div>
         </Router>
